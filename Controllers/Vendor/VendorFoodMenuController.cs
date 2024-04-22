7ddd117e340bd563979283b11bfb77e659eebf5f
mohammadzarkasi <mohammad.zarkasi@gmail.com>
2024-04-22 17:38:35 +0700

    test mengambil user dari jwt token

--- Controllers/Vendor/VendorFoodMenuController.cs
+++ Controllers/Vendor/VendorFoodMenuController.cs
@@ -1,22 +1,32 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using smartkantin.Models;
 using smartkantin.Repository;
+using smartkantin.Tools;
 
 namespace smartkantin.Controllers.Vendor;
 
 [ApiController]
 [Route("/api/vendor/food")]
+[Authorize]
 public class VendorFoodMenuController : ControllerBase
 {
     private readonly IFoodRepository foodRepository;
+    private readonly UserManager<AppUser> userManager;
 
-    public VendorFoodMenuController(IFoodRepository foodRepository)
+    public VendorFoodMenuController(IFoodRepository foodRepository, UserManager<AppUser> userManager)
     {
         this.foodRepository = foodRepository;
+        this.userManager = userManager;
     }
+
     [HttpGet]
-    public string GetAll()
+    public async Task<IActionResult> GetAll()
     {
-        return "get all";
+        var user = await SessionTools.GetCurrentUser(userManager, User);
+        // return Ok("get all");
+        return Ok(user);
     }
 
     [HttpPost("add")]
---
+++ Tools/SessionTools.cs
@@ -0,0 +1,18 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using smartkantin.Models;
+
+namespace smartkantin.Tools
+{
+    public class SessionTools
+    {
+        internal static async Task<AppUser?> GetCurrentUser(UserManager<AppUser> userManager, ClaimsPrincipal claimPrincipal)
+        {
+            var userIdClaim = claimPrincipal.Claims.Where(claim => claim.Type == "user_id").FirstOrDefault();
+            var userId = userIdClaim?.Value ?? "";
+            var appUser = await userManager.FindByIdAsync(userId);
+
+            return appUser;
+        }
+    }
+}
